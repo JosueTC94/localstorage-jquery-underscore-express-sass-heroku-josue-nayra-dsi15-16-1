b6082bc3336afbacffc7a601ab7d28d874d63ba9
alu0100406122 <alu0100406122@ull.edu.es>
2016-04-11 10:29:32 +0000

    Prueba Heroku

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,14 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var calculate = require('../../assets/js/csv.js');
+// Función calculate ---------------------
+
+
 
 /* GET home page. */
+//Se invoca el index. Utilizamos plantilla (/views/index.ejs)
 router.get('/', function(req, res, next) {
   res.render('index', { title: "Practica 7", title1: "Comma separated values"});
 });
 
+//req.body --> capturar
+//res.render --> redirige
+
 router.post('/csv',function(req,res,next)
 {
     var contenido = req.body.original;
-    res.render('resultado', { resultado: contenido });
+    var r = calculate(contenido);
+    res.render('resultado', { resultado: r });
 });
 module.exports = router;
