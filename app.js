bb2110a89819f4a0b8cabf09e521c7a941b7f3c0
alu0100406122 <alu0100406122@ull.edu.es>
2016-04-15 09:53:39 +0000

    Seguimos probando...

--- app.js
+++ app.js
@@ -1,9 +1,70 @@
-var express = require('express');
+
+var express = require('express')
+var app = express()
+var csv = require('./csv.js')
+var util = require('util')
+
+var path = require('path');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+var expressLayouts = require('express-ejs-layouts');
+app.set('layout', 'layout');
+
+app.use(express.static('public'));
+app.use(expressLayouts);
+
+app.set('port', (process.env.PORT || 8080)); 
+
+/*
+ * body-parser is a piece of express middleware that 
+ *   reads a form's input and stores it as a javascript
+ *   object accessible through `req.body` 
+ *
+ * 'body-parser' must be installed (via `npm install --save body-parser`)
+ * For more info see: https://github.com/expressjs/body-parser
+ */
+
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.get('/', function(req, res){
+  res.render('index', { title: "Comma Separated Value Analyzer", error:""});
+});
+
+app.post('/table', function(req, res, next){
+    var original = req.body.original;
+    if(!original){ 
+      res.render('index', { title: "Comma Separated Value Analyzer", error: "Introduzca datos de entrada, por favor"});
+    }else{
+      var data = csv.calculate(original);
+      res.render('table', {items: data, title: "Comma Separated Value Analyzer", error:""});
+    }
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
+
+
+
+/*var express = require('express');
 var app = express();
 var csv = require('assets/js/csv.js');
 var util = require('util');
 
 var path = require('path');
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+var expressLayouts = require('express-ejs-layouts');
+app.set('layout', 'layout');
+
+
+
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
@@ -11,12 +72,6 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');  
-
-
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -64,3 +119,4 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
+*/
